84801906ed5db66f95e62ba1c918cc95abefef44
Lillian Luzinsky <eyeris@tpg.com.au>
2019-11-30 14:25:53 +1100

    display fight number from json

--- burning_airlines_app/src/components/Planes.js
+++ burning_airlines_app/src/components/Planes.js
@@ -2,33 +2,33 @@ import React from "react";
 import axios from "axios";
 
 export default class Planes extends React.Component {
-  constructor() {
-    super();
-    this.state = { planesList: [] };
+  constructor(props) {
+    super(props);
+    this.state = {
+      planesList: []
+    };
   }
+
   componentDidMount() {
-    const serverURL = `http://localhost:3000/planes`;
+    const serverURL = `http://localhost:3000/planes.json`;
     axios.get(serverURL).then(res => {
       const myData = res.data;
       this.setState({
-        myData
+        planesList: myData
       });
-      console.log(res);
     });
   }
+
   render() {
     const myDatabase = this.state.planesList.map((s, id) => (
-      <li key={id}> {s.name} </li>
-    ));
-    const myDate = this.state.planesList.map((j, id) => (
-      <li key={id}> {j.created_at} </li>
+      <>
+        <li key={id}> {s.name} </li>
+        <li key={id}> {s.created_at} </li>
+      </>
     ));
     return (
       <div>
-        <ul>
-          {myDatabase}
-          {myDate}
-        </ul>
+        <ul>{myDatabase}</ul>
       </div>
     );
   }
