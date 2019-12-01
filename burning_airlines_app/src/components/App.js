aadd7efc83b1e4890aea09c55d765f42fe969852
Lillian Luzinsky <eyeris@tpg.com.au>
2019-12-01 18:26:31 +1100

    creating new routers

--- burning_airlines_app/src/components/App.js
+++ burning_airlines_app/src/components/App.js
@@ -5,13 +5,20 @@ import HomePage from "../pages/HomePage";
 import Flights from "../pages/Flights";
 import FlightDetails from "../pages/FlightDetails";
 import SearchFlight from "../pages/SearchFlight";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <p>THIS IS A TITLE FROM APP COMPONENT</p>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" component={HomePage} />
+          <Route path="/flights" component={Flights} />
+        </Switch>
+        <p>THIS IS A TITLE FROM APP COMPONENT</p>
+      </div>
+    </Router>
   );
 }
 
