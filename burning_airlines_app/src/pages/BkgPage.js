a725f57a3cf6d6257e21feb23bd9b0818e9972e1
Gabriel Conceicao <gabrielcon6cao@gmail.com>
2019-12-06 20:19:14 +1100

    able to make post requests!

--- burning_airlines_app/src/pages/BkgPage.js
+++ burning_airlines_app/src/pages/BkgPage.js
@@ -13,18 +13,21 @@ export default class BkgPage extends Component {
     
     handleSubmit = event => {
         event.preventDefault();
-    
+        console.log(this.state)
         const user = {
             passenger_name: this.state.passenger_name,
             flight_id: 1,
             seat_number: "2C",
             user_id: 1,
+            
         }
-        axios.post(`http://localhost:3000/reservations`, { user }).then(res => {
+        axios.post(`http://localhost:3000/reservations.json`, user).then(res => {
             console.log(res);
             console.log(res.data);
         
-        })
+        }).catch(error => {
+            console.log(error.response)
+        });
     }
     
     render () {
@@ -34,12 +37,12 @@ export default class BkgPage extends Component {
                 <label>
                     Passenger Name:<br />
                     <input type="text" passenger_name="passenger_name" onChange={this.handleChange}/><br />
-                    {/* Flight ID:<br /> */}
-                    {/* <input type="integer" value="1" flight_id="flight_id" onChange={this.handleChange}/><br />
+                    Flight Number:<br />
+                    <input type="integer" value="VA123" readOnly /><br />
                     Seat Number:<br />
-                    <input type="text" value="2C" seat_number="seat_number" onChange={this.handleChange}/><br />
-                    User ID:<br />
-                    <input type="integer" value="1" user_id="user_id" onChange={this.handleChange}/><br /> */}
+                    <input type="text" value="2C" readOnly /><br />
+                    Date:<br />
+                    <input type="integer" value="20/12/2019" readOnly /><br />
                 </label>
                 <button type="submit">Add</button>
             </form>
